667cb0dc3c48bcd0a4decaea40852b821d2b3617
J Delaney <j.delaney.sd@gmail.com>
2015-02-18 13:34:06 -0800

    Hogan.js added for templating

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'hjs');
+
 app.get('/', function (req, res) {
-    res.send('Hello world');
+    res.render('stage1', {
+        test: 'Hello world'
+    });
 });
 
 var server = app.listen(3000, function () {
